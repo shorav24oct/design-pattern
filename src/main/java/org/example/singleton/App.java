7528ea3899ef9924c4542c8be425fed22557a537
shorav24oct <sharmasaurav142@gmail.com>
2026-01-01 07:42:09 +0530

    break singleton using reflection

--- src/main/java/org/example/singleton/App.java
+++ src/main/java/org/example/singleton/App.java
@@ -1,12 +1,15 @@
 package org.example.singleton;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args ) throws CloneNotSupportedException {
+    public static void main( String[] args ) throws CloneNotSupportedException, InvocationTargetException, InstantiationException, IllegalAccessException {
         System.out.println( "Hello World!" );
 
         LazySingleton lazySingleton = LazySingleton.getInstance();
@@ -14,8 +17,14 @@ public class App
 
         //LazySingleton lazySingleton1 = (LazySingleton) lazySingleton.clone();
 
+        LazySingleton reflectionInstance = null;
+        Constructor[] constructors = LazySingleton.class.getDeclaredConstructors();
 
+        for (Constructor constructor : constructors) {
+            constructor.setAccessible(true);
+            reflectionInstance = (LazySingleton) constructor.newInstance();
+        }
 
-        //System.out.println(lazySingleton1.hashCode());
+        System.out.println(reflectionInstance.hashCode());
     }
 }
